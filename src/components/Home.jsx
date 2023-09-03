d69bbd4fd59b3d817b6197ca99667d52c81b4247
JimChristian21 <jimchristianedullantes9@gmail.com>
2023-09-03 17:26:05 +0800

    Added home section, profile and updated classes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import profile from "../images/profile.png";
 
 const Home = () => {
 
     return (
-        <h1> Test</h1>
+        <div id="Home" className="w-full color-3 flex">
+            <div className='mx-auto md:mt-20 lg:mt-40 w-6/12 md:pl-40 lg:pl-64'>
+                <p className="text-white md:text-3xl lg:text-4xl">
+                    Hi, my name is...
+                </p>
+                <p className="font-bold md:text-7xl lg:text-9xl">
+                    Jim Christian <br/> Edullantes
+                </p>
+                <p className="text-white md:text-2xl lg:text-3xl">
+                    Software Engineer
+                </p>
+                <p className="text-xl text-white mt-3">
+                    I build things for the web that help businesses achieve its goal. <br /> 
+                    Create and build solutions that will satisfy your needs.  <br /> 
+                    Provided solutions and applications are well planned and analyzed.
+                </p>
+            </div>
+            <div className='mx-auto w-6/12'>
+                <img className="w-10/12" src={profile} alt="profile"/>
+            </div>
+        </div>
     );
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     const classes = 'mx-5 p-2 color-3 border-b-2 border-color-3 hover:bg-slate-700 rounded-t-md font-medium transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-500';
 
     return (
-        <div className="w-full h-14 flex justify-center items-center text-2xl mt-5">
+        <div className="w-full h-14 flex justify-center items-center text-2xl my-4 sticky top-0">
             <a href="#Home" className={classes}>Home</a>
             <a href="#Projects" className={classes}>Projects</a>
             <a href="#AboutMe" className={classes}>About Me</a>
